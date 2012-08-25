2f4c9bea10c4fa67e28659a972f65da6dfef56f5
Jonathan Lange <jml@mumak.net>
2012-08-25 13:03:42 +0100

    Don't need plugins anymore.

--- local/plugins.el
+++
@@ -1,9 +0,0 @@
-;; Load third-party plugins that aren't managed by 'package'.
-(provide 'plugins)
-
-;; Where all of my third-party plugins are installed.
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/plugins"))
-
-(require 'package) ;; NB: Used for package management.
-(require 'rst)
-
