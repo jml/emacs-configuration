62a97296eb99b8156d06a9c7bb5b27c05a1fe1a4
Jonathan Lange <jml@mumak.net>
2012-08-25 12:46:15 +0100

    Refer to functions correctly.

--- local/keybindings.el
+++ local/keybindings.el
@@ -5,5 +5,5 @@
 (global-set-key "\C-m" 'newline-and-indent) ;; enter key indents
 (global-set-key "\M-j" (lambda () (interactive) (delete-indentation 1)))
 (global-set-key "\C-x\C-b" 'ibuffer-other-window)
-(global-set-key "\M-n" (scroll-up-line))
-(global-set-key "\M-p" (scroll-down-line))
+(global-set-key "\M-n" 'scroll-up-line)
+(global-set-key "\M-p" 'scroll-down-line)
