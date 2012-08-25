a7d1bedd79df154c0d6440bbf6c1a9e76183f5a5
Jonathan Lange <jml@mumak.net>
2012-08-25 12:32:12 +0100

    Emacs 24 comes with this function built in.

--- local/keybindings.el
+++ local/keybindings.el
@@ -5,5 +5,5 @@
 (global-set-key "\C-m" 'newline-and-indent) ;; enter key indents
 (global-set-key "\M-j" (lambda () (interactive) (delete-indentation 1)))
 (global-set-key "\C-x\C-b" 'ibuffer-other-window)
-(global-set-key "\M-n" (lambda () (interactive) (scroll-up 1)))
-(global-set-key "\M-p" (lambda () (interactive) (scroll-down 1)))
+(global-set-key "\M-n" (scroll-up-line))
+(global-set-key "\M-p" (scroll-down-line))
