bab22176ede060e3fd131395e1af341970fd043c
Jonathan Lange <jml@mumak.net>
2012-08-25 12:31:47 +0100

    Whitespace changes.

--- programming/clojure.el
+++ programming/clojure.el
@@ -1,6 +1,7 @@
 (require 'clojure-mode)
 
-(defun turn-on-paredit () (paredit-mode +1))
+(defun turn-on-paredit ()
+  (paredit-mode +1))
 
 (add-hook 'clojure-mode-hook 'turn-on-paredit)
 (add-hook 'slime-repl-mode-hook 'turn-on-paredit)
