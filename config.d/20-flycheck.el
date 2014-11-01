9bfe88544bc85cff1b924390171b89b05e64e395
Jonathan Lange <jml@mumak.net>
2014-11-01 09:24:15 +0000

    Fix up flycheck configuration for rust

--- config.d/20-flycheck.el
+++ config.d/20-flycheck.el
@@ -1,5 +1,8 @@
 ;; Enable flycheck globally
 
-(add-hook 'after-init-hook #'global-flycheck-mode)
+(eval-after-load 'flycheck
+  '(progn
+     (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+     (add-hook 'flycheck-mode-hook #'flycheck-haskell-setup)))
 
-(add-hook 'flycheck-mode-hook #'flycheck-haskell-setup)
+(add-hook 'after-init-hook #'global-flycheck-mode)
