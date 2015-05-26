d06b4c5c7fd7c1af22e1aa9f01a9c15fdc9007f3
Jonathan Lange <jml@mumak.net>
2015-05-26 09:36:13 +0100

    Bind helm-projectile

--- config.d/30-projectile.el
+++ config.d/30-projectile.el
@@ -2,6 +2,7 @@
 
 (req-package projectile
   :require (helm-projectile helm-ag)
+  :bind (("C-c h" . helm-projectile))
   :config (progn
             (projectile-global-mode)
             (helm-projectile-on)))
