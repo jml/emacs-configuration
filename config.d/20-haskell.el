6d4e8c549d71e1cbda586e6dc7af21316fc4d768
Jonathan Lange <jml@mumak.net>
2016-06-19 14:47:24 +0100

    Use direnv & nix to set flycheck ghc variables

--- config.d/20-haskell.el
+++ config.d/20-haskell.el
@@ -1,5 +1,6 @@
 
 (req-package haskell-mode
-  :require (haskell-process)
+  :require (haskell-process ghc-nix)
   :config (progn
-            (add-hook 'haskell-mode-hook #'turn-on-haskell-indentation)))
+            (add-hook 'haskell-mode-hook #'turn-on-haskell-indentation)
+            (add-hook 'haskell-mode-hook #'use-nix-ghc-in-flycheck)))
--- plugins/ghc-nix.el
+++ plugins/ghc-nix.el
@@ -11,50 +11,63 @@
   (add-dir-local-variable 'haskell-mode 'flycheck-ghc-package-databases (nix-package-databases))
   (add-dir-local-variable 'haskell-mode 'flycheck-haskell-ghc-executable (nix-ghc-executable)))
 
+(defun use-nix-ghc-in-flycheck ()
+  "Configure the current buffer to use ghc provided by the containing nix shell."
+  (interactive)
+  (if (locate-dominating-file default-directory "shell.nix")
+      (progn
+        (setq flycheck-ghc-package-databases nil)
+        (setq flycheck-haskell-ghc-executable (nix-ghc-executable))) nil))
+
 (defun nix-package-databases ()
   "Return a list of ghc package databases under nix."
-  (-find-package-databases (-run-nix-command "ghc-pkg list")))
+  (-find-package-databases (-run-nix-command "$(type -p ghc-pkg) list")))
 
 (defun nix-ghc-executable ()
   "Return the path to the ghc executable inside the nix-shell."
-  (-chomp-end (-run-nix-command "which ghc")))
+  (-chomp-end (-run-nix-command "type -p ghc")))
+
 
 (defun -run-nix-command (command)
   "Run COMMAND inside a nix-shell, returning output."
   (let ((default-directory (locate-dominating-file default-directory "shell.nix")))
-    (shell-command-to-string (-get-nix-command command))))
+    (shell-command-to-string (-get-command-for-env default-directory command))))
+
+(locate-dominating-file "/home/jml/src/holborn/holborn-repo/lib/Holborn" "shell.nix")
+(-get-command-for-env "/home/jml/src/holborn/holborn-repo/lib/Holborn" "$(type -p ghc-pkg) list")
+
+(defun -get-command-for-env (directory command)
+  "Get the shell command to run COMMAND with correct environment."
+  (let ((direnv-directory (locate-dominating-file directory ".envrc")))
+    (if direnv-directory
+        (-get-direnv-nix-command direnv-directory command) (-get-nix-command command))))
 
 (defun -get-nix-command (command)
   "Get the command to run COMMAND inside a nix-shell."
   (combine-and-quote-strings `("nix-shell" "--run" ,command)))
 
-
-
+(defun -get-direnv-nix-command (directory command)
+  "Get the command to run COMMAND inside a nix-shell using direnv variables."
+  (combine-and-quote-strings `("direnv" "exec" ,directory "$(type -p nix-shell)" "--command" ,command "2>/dev/null")))
 
 
 (defun -find-package-databases (ghc-pkg-output)
   "Find package database paths in GHC-PKG-OUTPUT."
   (-map-maybe '-package-database-path (split-string ghc-pkg-output)))
 
-
 (defun -package-database-p (s)
   "Is S a package database?"
-  (char-equal (-last-element s) ?:))
-
+  (char-equal (-first-element s) ?/))
 
 (defun -package-database-path (s)
   "Return path to package database represented by S.
 
 If not a package database, return nil."
-  (if (-package-database-p s)
-      (substring s 0 -1) nil))
+  (if (-package-database-p s) s nil))
 
-
-
-(defun -last-element (s)
+(defun -first-element (s)
   "Find the last element of S."
-  (elt s (- (length s) 1)))
-
+  (elt s 0))
 
 (defun -map-maybe (f xs)
   "Map F over XS, discarding when f(x) is nil."
