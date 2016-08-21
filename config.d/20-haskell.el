b486a51613d15e5dcf787a949c7fa21cce9cb7d3
Jonathan Lange <jml@mumak.net>
2016-08-21 11:47:33 +0100

    Johan Tibell's haskell-style

--- config.d/20-haskell.el
+++ config.d/20-haskell.el
@@ -1,6 +1,13 @@
 
 (req-package haskell-mode
-  :require (haskell-process ghc-nix)
+  :require (haskell-process ghc-nix haskell-style)
   :config (progn
             (add-hook 'haskell-mode-hook #'turn-on-haskell-indentation)
             (add-hook 'haskell-mode-hook #'use-nix-ghc-in-flycheck)))
+
+
+;; See also (add-hook 'haskell-mode-hook #'haskell-style) for switching on johanTibell style
+
+;; TODO: hlint set up
+;; TODO: hindent set up
+;; TODO: intero (c.f. tom)
