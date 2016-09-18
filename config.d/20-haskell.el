106e3159b724c6b46fed68e95adaa00860c5346f
Jonathan Lange <jml@mumak.net>
2016-09-18 15:46:23 +0100

    Intero + Hlint

--- config.d/20-haskell.el
+++ config.d/20-haskell.el
@@ -1,9 +1,12 @@
 
 (req-package haskell-mode
-  :require (haskell-process ghc-nix haskell-style)
+  :require (haskell-process ghc-nix haskell-style intero)
   :config (progn
             (add-hook 'haskell-mode-hook #'turn-on-haskell-indentation)
-            (add-hook 'haskell-mode-hook #'use-nix-ghc-in-flycheck)))
+            (add-hook 'haskell-mode-hook #'use-nix-ghc-in-flycheck)
+            (global-intero-mode)
+            (flycheck-add-next-checker 'intero
+                                       '(warning . haskell-hlint))))
 
 
 ;; See also (add-hook 'haskell-mode-hook #'haskell-style) for switching on johanTibell style
