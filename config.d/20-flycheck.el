f43c1bffa5ae574cd3b00db7eeb07181aeaa0eb0
Jonathan Lange <jml@mumak.net>
2018-10-05 13:49:25 +0100

    Emacs 26.1 upgrade
    To be honest, losing control of my config at this point, but it definitely
    works on my laptop.

--- config.d/20-flycheck.el
+++ config.d/20-flycheck.el
@@ -1,12 +1,10 @@
 ;; Enable flycheck globally
 
 (req-package flycheck
-  :require (flycheck-purescript)
   :config
   (progn
     (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
     (add-hook 'flycheck-mode-hook #'flycheck-haskell-setup)
-    (add-hook 'flycheck-mode-hook #'flycheck-purescript-setup)
     (add-hook 'after-init-hook #'global-flycheck-mode)))
 
 (req-package flycheck-gometalinter
--- config.d/20-purescript.el
+++
@@ -1,3 +0,0 @@
-(req-package purescript-mode
-  :require (psci)
-  :config (add-hook 'purescript-mode-hook #'haskell-indentation-mode))
